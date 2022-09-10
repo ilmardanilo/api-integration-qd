ed54b8cc63baf0b6808364f6368a07d62b6c2597
Ilmar <ilmardanilo@hotmail.com>
2022-09-10 15:35:31 -0300

    Criado instancias para acesso as duas API com axios

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+require('dotenv').config()
+
+const apiSystemMercado = axios.create({
+    baseURL: `https://challenge-quero-delivery.herokuapp.com/${process.env.PLACE_ID}/products`,
+    headers: {
+        Authorization: `Basic ${process.env.SM_TOKEN}`
+    }
+})
+
+const apiQueroDelivery = axios.create({
+    baseURL: 'https://stageapi.quero.io',
+    headers: {
+        Authorization: `Basic ${process.env.QD_TOKEN}`
+    },
+    params: {
+        placeId: process.env.PLACE_ID
+    }
+})
 
 app.listen(8080, () => console.log("Server ON"))
